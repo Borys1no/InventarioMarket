fcbed09ee0505ec2c26374ea41c27c4bfe5729f5
borys1no <boryscereceda@hotmail.com>
2023-02-14 12:13:00 -0500

    Actualizacion: desarrollo de la ventana login

--- src/main/java/com/mycompany/inventariomarket/InventarioMarket.java
+++ src/main/java/com/mycompany/inventariomarket/InventarioMarket.java
@@ -11,6 +11,6 @@ package com.mycompany.inventariomarket;
 public class InventarioMarket {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
